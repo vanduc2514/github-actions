dc979782a454a486036379467c5c70eb8636b279
vanduc2514 <vanduc2514@gmail.com>
2022-09-24 11:51:47 +0700

    Add hello world

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello CodeGymDay
         </a>
       </header>
     </div>
